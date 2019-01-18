625e0d5fde25e2a03293dd4d8212224a58ee0b50
abishabenjamin <31510910+abishabenjamin@users.noreply.github.com>
2019-01-18 18:13:42 +0530

    3

---
+++ number.java
@@ -0,0 +1,15 @@
+public class number{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n==0){
+System.out.println("number is zero");
+}
+if(n>0){
+System.out.println("number is positive");
+}
+else{
+System.out.println("number is negative");
+}
+}
+}
