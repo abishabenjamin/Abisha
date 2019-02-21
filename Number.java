6e5517f9442c7a7eba2677eaa668fe25bcdf289a
abishabenjamin <31510910+abishabenjamin@users.noreply.github.com>
2019-02-21 18:00:10 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Number {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n==0){
+            System.out.println("zero");
+        }
+       else if(n<=0){
+            System.out.println("negative");
+        }
+        else{
+        System.out.println("positive");
+        }
+        }
+    }
