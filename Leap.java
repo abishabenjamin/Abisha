6f01496f0dfa3f4850abadebb6165a1b16e410ac
abishabenjamin <31510910+abishabenjamin@users.noreply.github.com>
2019-02-21 18:54:21 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Leap {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+       int a=sc.nextInt();
+       if(a%4==0){
+           System.out.println("yes");
+       }
+       else{
+           System.out.println("no");
+       }
+    }
+    }
+
