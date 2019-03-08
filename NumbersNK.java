11b17f54166c1ebeb8d4af6618c698eb66d16e97
abishabenjamin <31510910+abishabenjamin@users.noreply.github.com>
2019-03-08 17:24:45 +0530

    Create NumbersNK.java

---
+++ NumbersNK.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class NumbersNK{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int N=sc.nextInt();
+int K=sc.nextInt();
+int a=0;
+for(int i=0;i<=k;i++){
+a=a+i;
+}
+System.out.println(a);
+}
+}
