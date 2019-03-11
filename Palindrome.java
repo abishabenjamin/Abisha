77514141b574191bf23f5404b25e8a3d75763209
abishabenjamin <31510910+abishabenjamin@users.noreply.github.com>
2019-03-11 17:13:06 +0530

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Palindrome{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int a=n;
+int rem=0;
+int c=0;
+while(n!=0){
+rem=n%10;
+c=c*10+rem;
+n=n/10;
+}
+if(num==z){
+System.out.println("yes");
+}
+else{
+System.out.println("no");
+}
+}
+}
