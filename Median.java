1b033d5f4d02cda66a9903c5fde175f6007b75aa
abishabenjamin <31510910+abishabenjamin@users.noreply.github.com>
2019-05-02 15:11:16 +0530

    Update Median.java

--- Median.java
+++ Median.java
@@ -3,22 +3,36 @@ public class Median
 {
     public static void main(String[] args) 
     {
-        int n;
+        int n, temp;
         Scanner s = new Scanner(System.in);
        
         n = s.nextInt();
         int a[] = new int[n];
-        int b=0;
+        int l=n/2;
         for (int i = 0; i < n; i++) 
         {
             a[i] = s.nextInt();
         }
         for (int i = 0; i < n; i++) 
         {
-            b =a[i]+ b;
-          
+            for (int j = i + 1; j < n; j++) 
+            {
+                if (a[i] > a[j]) 
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
         }
-        System.out.print(b/n);
+    
+        for (int i = 0; i < n - 1; i++) 
+        {
+        }
+            System.out.print(a[l] );
+        
+       
     }
 }
+
 
