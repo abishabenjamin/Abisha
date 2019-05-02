c2feea3a839908fca3d536b24390f13c3620f409
abishabenjamin <31510910+abishabenjamin@users.noreply.github.com>
2019-05-02 11:55:28 +0530

    Create First5multi.java

---
+++ First5multi.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class First5multi {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int fact=1;
+    for(int i=1;i<=5;i++){
+         fact=n1*i;
+    
+        
+System.out.print(fact + " ");
+}
+}
+}
+
+
+
