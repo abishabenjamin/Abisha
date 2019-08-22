5371f702008e314ed97e454794f447cd657917c7
abishabenjamin <31510910+abishabenjamin@users.noreply.github.com>
2019-08-22 09:49:21 +0530

    Create PatternForm.java

---
+++ PatternForm.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+public class MyClass {
+   public static void main(String args[]) {
+       Scanner sc=new Scanner(System.in);
+       int n=sc.nextInt();
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=1;j<=n;j++)
+           {
+               if(i==1||i==n||j==1||j==n || i==j && j!=(n/2)+1 || j==(n+1)-i && j!=(n/2)+1  )
+       {
+           System.out.print(" *");
+       }
+         else
+       {
+           System.out.print(" "+"-");
+       }        
+           }
+    
+        System.out.println();
+       }   
+      
+   }
+}
